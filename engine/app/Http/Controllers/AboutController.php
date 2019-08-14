08f8f32e3dcd6c8c81cf42a091f1c71f7c0127db
Ariq Daffa <ariqdaffaathallah@gmail.com>
2019-08-14 12:39:34 +0700

    tambah map

--- engine/app/Http/Controllers/AboutController.php
+++ engine/app/Http/Controllers/AboutController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mapper;
 
 class AboutController extends Controller
 {
     public function index()
     {
-        return view('about.index');
+        // $map = Mapper::marker(0.4916589, 101.4538533);
+        $map = Mapper::map(53.381128999999990000, -1.470085000000040000);
+        // dd($map);
+
+        return view('about.index', compact('map'));
     }
 }
